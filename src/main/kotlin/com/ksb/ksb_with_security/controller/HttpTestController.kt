6dc6d9789565660105336a78c9909063801c5e72
jack <guangjian.cgj@alibaba-inc.com>
2018-02-02 02:57:56 +0800

    进阶 Security： 基于数据库的用户和角色权限

--- src/main/kotlin/com/ksb/ksb_with_security/controller/HttpTestController.kt
+++ src/main/kotlin/com/ksb/ksb_with_security/controller/HttpTestController.kt
@@ -21,7 +21,7 @@ class HttpTestController {
     @PostMapping("/httpTest")
     fun doTest(@Valid httpTestRequest: HttpTestRequest, bindingResult: BindingResult): HttpTestResult {
         val request = (RequestContextHolder.getRequestAttributes() as ServletRequestAttributes).request
-//        ${(Session["SPRING_SECURITY_CONTEXT"].authentication.principal.username)!}
+        // ${(Session["SPRING_SECURITY_CONTEXT"].authentication.principal.username)!}
         // 从 Spring Security 当前 session 中获取 SPRING_SECURITY_CONTEXT 对象
         val authentication = (request.session.getAttribute("SPRING_SECURITY_CONTEXT") as SecurityContext).authentication
         val username = (authentication.principal as User).username
--- src/main/kotlin/com/ksb/ksb_with_security/service/MyUserDetailService.kt
+++ src/main/kotlin/com/ksb/ksb_with_security/service/MyUserDetailService.kt
@@ -15,14 +15,20 @@ class MyUserDetailService : UserDetailsService {
     @Autowired lateinit var userDao: UserDao
 
     override fun loadUserByUsername(username: String): UserDetails {
+        // 去数据库 User 表，根据 username 查询用户是否存在
         val user = userDao.findByUsername(username) ?: throw  UsernameNotFoundException(username + " not found")
         logger.info("user = {}", user)
-        val roles = user.roles
+        val roles = user.roles // 数据库中存的该用户的角色集合
+        /**
+         * SimpleGrantedAuthority: Basic concrete implementation of a GrantedAuthority
+         */
         val authorities = mutableSetOf<SimpleGrantedAuthority>()
         roles.forEach {
+            // 封装 org.springframework.security.core.userdetails.User 对象中的
+            // Collection<? extends GrantedAuthority> authorities 参数
             authorities.add(SimpleGrantedAuthority(it.role))
         }
-        return org.springframework.security.core.userdetails.User( // 此处为了区分我们本地系统中的 User 实体类，特意列出userdetails 的 User 类的全路径
+        return org.springframework.security.core.userdetails.User( // 此处为了区分我们本地系统中的 User 实体类，特意列出 Security 中的 User 类的全路径
                 username,
                 user.password,
                 authorities
